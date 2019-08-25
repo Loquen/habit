59581296bfcf30d6538a8429e0f5dfd3eefe1b15
Loquen Jones <lrjones416@gmail.com>
2019-08-25 14:41:06 -0700

    post route from new habit page, creating new habit object to save to DB

--- controllers/habits.js
+++ controllers/habits.js
@@ -1,8 +1,10 @@
 const User = require('../models/user');
+const moment = require('moment');
 
 module.exports = {
   index,
-  new: newHabit
+  new: newHabit,
+  create
 };
 
 function index(req, res, next){
@@ -25,9 +27,28 @@ function index(req, res, next){
   });
 }
 
+// Render new habit page
 function newHabit(req, res, next){
   res.render('habits/new', {
     user: req.user,
     title: 'Add A New Habit'
   });
+}
+
+// Create a new habit
+function create(req, res, next){
+  // What info do we have?
+  // Grab current month
+  // create array of month.daysInMonth() with default False values
+  let newHabit = req.body;
+  let newMonth = {};
+  let month = new Date;
+  month = month.getMonth() + 1;
+  newMonth.month = month; 
+  let numberOfDays = moment(month).daysInMonth();
+  let days = new Array(numberOfDays).fill(false);
+  newMonth.days = days;
+  newHabit.months = new Array(newMonth);
+  console.log(newHabit);
+  res.redirect('/habits');
 }
--- routes/habits.js
+++ routes/habits.js
@@ -5,6 +5,7 @@ var habitsCtrl = require('../controllers/habits');
 // GET /all Habits
 router.get('/habits', habitsCtrl.index);
 router.get('/habits/new', habitsCtrl.new);
+router.post('/habits', habitsCtrl.create);
 
 
 module.exports = router;
