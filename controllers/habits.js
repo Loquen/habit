46d4abe7219895c2b4e5f780eb6cd75d09d061b2
Loquen Jones <lrjones416@gmail.com>
2019-08-26 12:20:55 -0700

    update habit and delete habit working

--- controllers/habits.js
+++ controllers/habits.js
@@ -4,7 +4,10 @@ const moment = require('moment');
 module.exports = {
   index,
   new: newHabit,
-  create
+  create,
+  delete: deleteHabit,
+  edit,
+  update
 };
 
 function index(req, res, next){
@@ -55,6 +58,57 @@ function create(req, res, next){
     })
   });
   // res.redirect('/habits');
+}
+
+function deleteHabit(req, res, next){
+  console.log(req.params.id);
+  User.findById(req.user.id, function(err, user){
+    if(err) {
+      return res.render('habits');
+    }
+
+    user.habits.forEach((habit,idx) => {
+      if(habit.id === req.params.id){
+        console.log(habit);
+        user.habits.splice(idx, 1);
+        user.save(function(err){
+          res.redirect('/habits');
+        });
+      }
+    });
+  });
+}
+
+function edit(req, res){
+  User.findById(req.user.id, function(err, user){
+    user.habits.forEach((habit, idx) => {
+      if(habit.id === req.params.id){
+        res.render('habits/edit', {
+          habit,
+          user: req.user,
+          title: 'Update Habit',
+          name: habit.name,
+          category: habit.category
+        });
+      }
+    })
+  });
+}
+
+function update(req, res){
+  User.findById(req.user.id, function(err, user){
+    // Loop through habits looking for req.params.id
+    user.habits.forEach((habit, idx) => {
+      if(habit.id === req.params.id){
+        habit.name = req.body.name;
+        habit.category = req.body.category;
+        // console.log(req.body.name, req.body.category);
+        user.save(function(err){
+          res.redirect('/habits');
+        });
+      }
+    });
+  });
 }
 
 /********** HELPER FUNCTIONS *********/
--- routes/habits.js
+++ routes/habits.js
@@ -5,7 +5,10 @@ var habitsCtrl = require('../controllers/habits');
 // GET /all Habits
 router.get('/habits', habitsCtrl.index);
 router.get('/habits/new', isLoggedIn, habitsCtrl.new);
+router.get('/habits/:id', isLoggedIn, habitsCtrl.edit);
 router.post('/habits', isLoggedIn, habitsCtrl.create);
+router.put('/habits/:id', isLoggedIn, habitsCtrl.update);
+router.delete('/habits/:id', isLoggedIn, habitsCtrl.delete);
 
 function isLoggedIn(req, res, next) {
   if ( req.isAuthenticated() ) return next();
